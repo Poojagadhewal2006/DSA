aaa6c743a0af42961ecdc6ac0837469a630368ee
Poojagadhewal2006 <poojagadhewal2006@gmail.com>
2025-06-15 20:10:46 +0530

    Add files via upload

---
+++ MaxMin.java
@@ -0,0 +1,21 @@
+public class MaxMin {
+    public static void main(String[] args) {
+        int arr[] = {10,3,4,8,9,5,3};
+        int max = arr[0];
+        int min = arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i] > max)
+            {
+                max =arr[i];
+
+            }
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        System.out.println("max is : " + max);
+        System.out.println("min is : " + min);
+    }
+}
