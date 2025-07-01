5ca20767f755ab7a3103eda2e62dac6803d1b96d
Poojagadhewal2006 <poojagadhewal2006@gmail.com>
2025-07-01 23:30:31 +0530

    Add files via upload

---
+++ ZeroMatrix.java
@@ -0,0 +1,53 @@
+public class ZeroMatrix {
+    public static void setZeroes(int [][] matrix)
+    {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean[] rowZero = new boolean[rows];
+        boolean[] colZero = new boolean[cols];
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(rowZero[i] || colZero[j])
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static void printMatrix(int[][] matrix)
+    { 
+      for(int [] row : matrix)
+      {
+        for(int val : row)
+        {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+      }   
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3},
+            {4,0,6},
+            {7,8,9}
+        };
+        System.out.println("Original Matrix : ");
+        printMatrix(matrix);
+        setZeroes(matrix);
+        System.out.println("MOdified Matrix : ");
+        printMatrix(matrix);
+    }
+}
